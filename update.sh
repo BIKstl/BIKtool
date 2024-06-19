487faa99b73ca056995ac719aab03f7aa7db5e87
BIKstl <109000691+BIKstl@users.noreply.github.com>
2024-06-19 08:06:06 -0400

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,55 @@
+#
+#          d8888b. d888888b db   dD d888888b  .d88b.   .d88b.  db      
+#          88  `8D   `88'   88 ,8P' `~~88~~' .8P  Y8. .8P  Y8. 88      
+#          88oooY'    88    88,8P      88    88    88 88    88 88      
+#          88~~~b.    88    88`8b      88    88    88 88    88 88      
+#          88   8D   .88.   88 `88.    88    `8b  d8' `8b  d8' 88booo. 
+#          Y8888P' Y888888P YP   YD    YP     `Y88P'   `Y88P'  Y88888P 
+#
+#                          0_0 code by BIKstl 0_0
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/BIKtool/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/BIKtool
+
+clear
+
+mkdir BIKtool
+
+clear
+
+cd BIKtool
+
+clear
+
+git clone https://github.com/BIKstl/BIKtool.git
+
+clear
+
+cd BIKtool
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
